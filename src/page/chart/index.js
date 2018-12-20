3a815b7ce790b121c6cef9deaa3fdaee18bd7628
woohs <wujun1204sc@outlook.com>
2018-12-20 23:41:03 +0800

    完成聊天室布局

--- src/page/chart/index.js
+++ src/page/chart/index.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Icon, Row, Col, Button, Input } from 'antd';
+import style from './style.css';
 
 const { Header, Footer, Content } = Layout;
+
+const HeaderLayer = () => (
+  <div className="header-row">
+    <Row gutter={16}>
+      <Col span={8} className="header-col-1"><Icon type="caret-left" /></Col>
+      <Col span={8} className="header-col-2">广场</Col>
+      <Col span={8} className="header-col-3"><Icon type="bars" /></Col>
+    </Row>
+  </div>
+)
+
+const FooterLayer = () => (
+  <div className="footer-row">
+      <Input />
+      <Button type="primary">发送</Button>
+  </div>
+)
+
+const ContentLayer = () => (
+  <div className="content-row">
+
+  </div>
+)
+
+
 
 class ChartRoom extends Component{
   render(){
     return (
       <div>
         <Layout>
-          <Header>
-            页头
-          </Header>
-          <Content>
-            内容
-          </Content>
-          <Footer>
-            页脚
-          </Footer>
+          <HeaderLayer />
+          <ContentLayer />
+          <FooterLayer />
         </Layout>
       </div>
     )
